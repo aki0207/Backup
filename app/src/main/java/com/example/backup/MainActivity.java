233db445804859fc8b1c7372cf44501db7b216dc
Ngamii Akihiro <forever.look.aki@docomo.ne.jp>
2018-12-02 17:13:23 +0900

    テスト用で日付け持たせた。

--- app/src/main/java/com/example/backup/MainActivity.java
+++ app/src/main/java/com/example/backup/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.backup;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,8 +17,11 @@ public class MainActivity extends Activity {
 
     Cursor c;
     SQLiteDatabase db;
-    SharedPreferences pref;
-    boolean flg = false;
+    TestOpenHelper helper;
+    //テスト用日付け
+    int year = 2018;
+    int month = 11;
+    int day = 3;
 
 
     @Override
@@ -23,54 +29,100 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TestOpenHelper helper = new TestOpenHelper(this);
-        db = helper.getReadableDatabase();
-        pref = getSharedPreferences("InsertFlg", Context.MODE_PRIVATE);
-        flg = pref.getBoolean("FlG", false);
+        if (helper == null) {
+            helper = new TestOpenHelper(this);
+
+        }
+        if (db == null) {
+            db = helper.getReadableDatabase();
+        }
+
 
         try {
 
-            //初回だけカラム追加を行なう
-            if (!flg) {
 
-                db.execSQL("insert into amount_used(date,category,price) values ('2018-1101','交遊費', 10000);");
-                db.execSQL("insert into amount_used(date,category,price) values ('2018-1101','生活費', 20000);");
-                db.execSQL("insert into amount_used(date,category,price) values ('2018-1101','飲食費', 30000);");
-                db.execSQL("insert into amount_used(date,category,price) values ('2018-1101','買い物費', 0);");
-
-                //フラグをtrueにし、以後このif文内の処理は行なわない
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putBoolean("InsertFlg", true);
-                editor.apply();
-
-            }
-            LinearLayout layout = new LinearLayout(this);
-            layout.setOrientation(LinearLayout.VERTICAL);
-
-
-
-
-           /* c = db.query("amount_used", new String[]{"category", "price"},
-                    null, null, null, null, null);*/
-
-           //交遊費
-           String sql = "select * from amount_used where category = ?";
-            c = db.rawQuery(sql,new String[]{"交遊費"});
+            //総額
+            String sql = "select sum(price) from amount_used";
+            c = db.rawQuery(sql, null);
             boolean isEof = c.moveToFirst();
             while (isEof) {
-                TextView tv = (TextView)findViewById(R.id.excursion_fee);
-                tv.setText(String.format("%d", c.getInt(2)));
+                TextView tv = (TextView) findViewById(R.id.the_amount);
+                tv.setText(String.format("%d", c.getInt(0)));
+                isEof = c.moveToNext();
+            }
+
+
+            //交遊費
+            sql = "select price from amount_used where category = ? and date = ?";
+            c = db.rawQuery(sql, new String[]{"交遊費","2018-1103"});
+            isEof = c.moveToFirst();
+            while (isEof) {
+                TextView tv = (TextView) findViewById(R.id.excursion_fee);
+                tv.setText(String.format("%d", c.getInt(0)) + "円");
                 isEof = c.moveToNext();
             }
 
             //生活費
-            c = db.rawQuery(sql,new String[]{"生活費"});
+            sql = "select price from amount_used where category = ?";
+            c = db.rawQuery(sql, new String[]{"生活費"});
             isEof = c.moveToFirst();
             while (isEof) {
-                TextView tv = (TextView)findViewById(R.id.cost_of_living);
-                tv.setText(String.format("%d", c.getInt(2)));
+                TextView tv = (TextView) findViewById(R.id.cost_of_living);
+                tv.setText(String.format("%d", c.getInt(0)) + "円");
                 isEof = c.moveToNext();
             }
+
+
+            //飲食費
+            c = db.rawQuery(sql, new String[]{"飲食費"});
+            isEof = c.moveToFirst();
+            while (isEof) {
+                TextView tv = (TextView) findViewById(R.id.food_cost);
+                tv.setText(String.format("%d", c.getInt(0)) + "円");
+                isEof = c.moveToNext();
+            }
+
+            //買い物費
+            c = db.rawQuery(sql, new String[]{"買い物費"});
+            isEof = c.moveToFirst();
+            while (isEof) {
+                TextView tv = (TextView) findViewById(R.id.shopping_expenses);
+                tv.setText(String.format("%d", c.getInt(0)) + "円");
+                isEof = c.moveToNext();
+            }
+
+            //その他1
+            c = db.rawQuery(sql, new String[]{"その他1"});
+            isEof = c.moveToFirst();
+            while (isEof) {
+                TextView tv = (TextView) findViewById(R.id.other_one);
+                tv.setText(String.format("%d", c.getInt(0)) + "円");
+                isEof = c.moveToNext();
+            }
+
+            //その他2
+            c = db.rawQuery(sql, new String[]{"その他2"});
+            isEof = c.moveToFirst();
+            while (isEof) {
+                TextView tv = (TextView) findViewById(R.id.other_two);
+                tv.setText(String.format("%d", c.getInt(0)) + "円");
+                isEof = c.moveToNext();
+            }
+
+
+            Button go_edit_button = (Button) findViewById(R.id.go_edit_page);
+            go_edit_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                    intent.putExtra("YEAR",year);
+                    intent.putExtra("MONTH",month);
+                    intent.putExtra("DAY",day);
+                    startActivity(intent);
+
+                }
+            });
 
 
         } catch (SQLiteException e) {
